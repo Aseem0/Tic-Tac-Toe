afd55aabb53c5b0d4d96e8ffd734b3e403448442
Aseem Rai <raiaseem25@gmail.com>
2025-07-06 23:56:43 +0545

    Tik Tac Toe

---
+++ script.js
@@ -0,0 +1,19 @@
+const boxes = document.querySelectorAll(".box");
+
+const winningPatterns = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    console.log("Button was clicked");
+    box.innerHTML = "X";
+  });
+});
